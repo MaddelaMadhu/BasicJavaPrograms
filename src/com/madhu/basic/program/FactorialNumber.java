325c207bb80caaef604f5e76023f2ded7a3f3d6c
= <venukumarmaddela@gmail.com>
2020-07-15 21:18:28 +0530

    fact

---
+++ src/com/madhu/basic/program/FactorialNumber.java
@@ -0,0 +1,27 @@
+package com.madhu.basic.program;
+
+import java.util.Scanner;
+
+public class FactorialNumber {
+
+	public static void main(String[] args) {
+		int n, fact = 1;
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("enter number");
+
+		n = sc.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+
+			fact = fact * i;
+
+		}
+
+		System.out.println("fact=" + fact);
+		sc.close();
+
+	}
+
+}
